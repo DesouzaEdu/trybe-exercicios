a0e96742ddb3f0033bb7953b6bb7979d5f7be399
Eduardo Souza <superedu2012@gmail.com>
2021-11-05 18:41:15 -0300

    Finalizando exercicios 4 e 5 dia 4.1

---
+++ 01 - Fundamentos/Bloco_4/4.1/programa_4.js
@@ -0,0 +1,13 @@
+const valor = 23;
+switch(valor){
+    case true:
+        console.log("positive");
+        break;
+    case false:
+        console.log("negative");
+        break;
+    default:
+        console.log("zero");
+        break;       
+        
+}
