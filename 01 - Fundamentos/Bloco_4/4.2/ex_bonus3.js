0ea494ccd505a17fab45ca69b17b54f0366fcaca
Eduardo Souza <superedu2012@gmail.com>
2021-11-09 16:57:11 -0300

    Começando dia 4.3

--- 01 - Fundamentos/Bloco_4/4.2/ex_bonus3.js
+++ 01 - Fundamentos/Bloco_4/4.2/ex_bonus3.js
@@ -2,10 +2,10 @@ let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = [];
 
 for (let index =0; index < array.length; index += 1) {
-    if(array[index+1]>0){
-        resultado.push(array[index]*array[index+1]);
-    }else{
+    if(array[index+1]===undefined){
         resultado.push(array[index]*2);
+    }else{
+        resultado.push(array[index]*array[index+1]);
     } 
   }
   console.log(resultado);
