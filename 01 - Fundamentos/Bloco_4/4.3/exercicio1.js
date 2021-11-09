c12b59638c82ca336608983c379110c274ac5733
Eduardo Souza <superedu2012@gmail.com>
2021-11-09 17:01:18 -0300

    Fazendo exercicio 1

---
+++ 01 - Fundamentos/Bloco_4/4.3/exercicio1.js
@@ -0,0 +1,10 @@
+let n =10;
+if(n>1){
+    for(let index = 0; index < n; index+=1){
+        let asteriscos = "";
+        for(let index = 0; index < n; index+=1){
+            asteriscos = asteriscos + "*";
+        }
+        console.log(asteriscos);
+    }
+}
