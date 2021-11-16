43ff34df32a0708748003866f0375b530d78ea05
Eduardo Souza <superedu2012@gmail.com>
2021-11-16 18:11:08 -0300

    Fazendo exercício 1 e 2 dia 5.1

---
+++ 01 - Fundamentos/Bloco_5/5.1/scriptFixacao.js
@@ -0,0 +1,6 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+document.querySelector('section').style.backgroundColor = '#ff9f84';
+document.querySelectorAll('section div')[0].style.backgroundColor = '#a500f3';
+document.querySelectorAll('section div')[1].style.backgroundColor = '#a500f3';
+document.querySelectorAll('section div')[0].style.paddingBottom = '10px';
+document.querySelectorAll('section div')[1].style.width = '#a500f3';
